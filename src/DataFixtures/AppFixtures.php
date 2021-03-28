152449d00ef47b95959b5b67b3a924d3f331d5c5
tham tran <thihanhtham.tran@gmail.com>
2021-03-29 00:52:32 +0200

    Symfony location

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,39 +3,49 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use App\Entity\Image;
+use Cocur\Slugify\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
-    {
-        
-    	for ($i=1; $i <=30 ; $i++) { 
+	public function load(ObjectManager $manager)
+	{
+		$slugify = new Slugify();
+		$titre = "titre de l'annonce n°!";
+		$slug = $slugify->slugify($titre);
 
-    		$ad = new Ad();
+		for ($i = 1; $i <= 30; $i++) {
 
-    		$ad->setTitle("Titre de l'annonce n° $i")
-    			-> setSlug("titre-de-l-annonce-n-$i")
-    			-> setCoverImage("https://via.placeholder.com/300")
-    			-> setIntroduction("<p>Introduction de l'annonce $i</p>")
-    			-> setContent("<p>Description de l'<b>annonce $i</b></p>")
-    			-> setPrice(mt_rand(40,150))
-    			-> setRooms(mt_rand(1,5));
+			$ad = new Ad();
 
-    		$manager->persist($ad);
-			$manager->flush();
+			$ad->setTitle("Titre de l'annonce n° $i")
+				->setSlug("$slug.$i")
+				->setCoverImage("https://via.placeholder.com/300")
+				->setIntroduction("<p>Introduction de l'annonce $i</p>")
+				->setContent("<p>Description de l'<b>annonce $i</b></p>")
+				->setPrice(mt_rand(40, 150))
+				->setRooms(mt_rand(1, 5));
 
-			$slug2=$ad->getSlug().'_'.$ad->getId();
+			// ajout d'image à une annonce
+			for ($j = 0; $j < mt_rand(2, 5); $j++) {
+				$image = new Image();
+				$image->setUrl('https://via.placeholder.com/300')
+					->setCaption("Légende de l'image $i")
+					->setAd($ad);
+				$manager->persist($image);
+			}
+
+			// boi vi sau etape tren thi chua co id vi annonce chua duoc tao ra trong bdd. Do do phai enregistrer annonce 1 lan trong bdd de tao ra id roi lai enregistre dans la bdd
+			$manager->persist($ad); // garde tout en mémoire
+			$manager->flush(); // enregistre tout dans la bdd
+
+			$slug2 = $ad->getSlug() . '_' . $ad->getId(); //ajoute Id dans le slug
 			$ad->setSlug($slug2);
 
 			$manager->persist($ad);
 			$manager->flush();
-
-    	}
-
-
-
-        
-    }
+		}
+	}
 }
